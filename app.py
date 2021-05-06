a5d7e7524ae0950346ed874be269443f71fc20b4
hongyiptang <56531970+hongyiptang@users.noreply.github.com>
2021-05-06 12:47:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def testing1():
-    return render_template('testing1.html')
+def testing2():
+    return render_template('testing2.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
