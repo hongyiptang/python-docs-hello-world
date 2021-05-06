d68672a0a230788ac4e5a03cd7271d23f6930225
hongyiptang <56531970+hongyiptang@users.noreply.github.com>
2021-05-06 12:20:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def testing1():
+    return render_template('testing1.html')
