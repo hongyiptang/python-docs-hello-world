0d26846b997180c250ec43890b711b058cab2cb2
hongyiptang <56531970+hongyiptang@users.noreply.github.com>
2021-05-06 14:21:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+
+# Referenciing this file
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template("testing1.html")
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
